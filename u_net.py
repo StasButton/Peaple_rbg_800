4b66480f5fdb2ce7b53207cf1cf17037fc3b2429
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-11 00:29:58 +0300

    Update u_net.py

--- u_net.py
+++ u_net.py
@@ -2,6 +2,9 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Conv2DTranspose, concatenate, Activation, MaxPooling2D, Conv2D, BatchNormalization 
 from tensorflow.keras.optimizers import Adam
 
+def dice_coef(y_true, y_pred):
+    return (2. * K.sum(y_true * y_pred) + 1.) / (K.sum(y_true) + K.sum(y_pred) + 1.)
+
 def modelUnet(num_classes = 2, input_shape= (1,256,192,3)):
     img_input = Input(input_shape)                                         # Создаем входной слой с размерностью input_shape
 
