55cf0bd9fd378af35acfcf04f0cd19cd734dd6c7
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-11 01:14:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,19 +40,18 @@ def load_image():
     else:
         return None
 
-#---<
+def prediction(i)
 Pred = []
 n_classes = 2
 #for inx, i in enumerate(xt): 
-  #pr = np.array(model.predict(i.reshape(1, img_height,img_width, 3))) # Предиктим картинку
-  #pr = pr.reshape(-1, 2) # Решейпим предикт
-  #pr1 = [] # Пустой лист под сегментированную картинку из predicta
-  
-  #for q in range(len(pr)): 
-   #   pr1.append(index2color(pr[q])) # Переводим индекс в писксель
-  #pr1 = np.array(pr1)
-  #pr1 = pr1.reshape(img_height,img_width,1)
- # Pred.append(pr1)
+pr = np.array(model.predict(i.reshape(1, img_height,img_width, 3))) # Предиктим картинку
+pr = pr.reshape(-1, 2) # Решейпим предикт
+pr1 = [] # Пустой лист под сегментированную картинку из predicta
+for q in pr: 
+   pr1.append(index2color(q) # Переводим индекс в писксель
+   pr1 = np.array(pr1)
+   pr1 = pr1.reshape(img_height,img_width,1)
+   Pred.append(pr1)
 #Pred = np.array(Pred)
 #--->
 
