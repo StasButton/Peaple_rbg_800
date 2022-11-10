803b18c5ca86be480c7effc22f6cbb53f631f814
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-10 20:33:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -147,8 +147,8 @@ def modelUnet(num_classes = 2, input_shape= (1,256,192,3)):
     return model
 
 model = modelUnet(num_classes,(img_height,img_width, 3))
-#--------------------------------------------------
 
+#--------------------------------------------------
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение')
     if uploaded_file is not None:
@@ -158,6 +158,25 @@ def load_image():
         return image_data 
     else:
         return None
+
+#---<
+Pred = []
+n_classes = 2
+#for inx, i in enumerate(xt): 
+  #pr = np.array(model.predict(i.reshape(1, img_height,img_width, 3))) # Предиктим картинку
+  #pr = pr.reshape(-1, 2) # Решейпим предикт
+  #pr1 = [] # Пустой лист под сегментированную картинку из predicta
+  
+  #for q in range(len(pr)): 
+   #   pr1.append(index2color(pr[q])) # Переводим индекс в писксель
+  #pr1 = np.array(pr1)
+  #pr1 = pr1.reshape(img_height,img_width,1)
+ # Pred.append(pr1)
+#Pred = np.array(Pred)
+#--->
+
+
+
 
 st.title('Загрузка, скачивание изображений')
 s = load_image()
