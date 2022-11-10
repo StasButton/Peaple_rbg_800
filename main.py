dda21420b9939adc1b5bc4aa0264dc499d947276
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-11 00:20:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tensorflow.keras.layers import Input, Conv2DTranspose, concatenate, Activat
 from tensorflow.keras.optimizers import Adam 
 from tensorflow.keras import utils
 
-import e
+import u_net
 
 img_width = 192
 img_height = 256
