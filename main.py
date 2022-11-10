db7c6a47baedb4be036ade9aeab75085af0367f4
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-11 01:07:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ result = st.button('Распознать изображение')
 if result:
     x = preprocess_image(img)
     x = preprocess_input(x)
-    st.text(type(x))
+    st.text(x.shape)
 
 #if s is not None:
  #   st.download_button(label='скачать',data=s,file_name = 'O.jpg')
