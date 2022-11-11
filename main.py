82a5685d45e8d1dc06bd02ce6db9d578e5884346
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-12 02:53:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ def load_image():
             st.image(image_data_bg)
             img_bg = Image.open(io.BytesIO(image_data_bg))
             x_bg = preprocess_image(img_bg)
+            x_bg = x_bg.reshape(-1, 3)
 
             result = st.button('Заменить фон')
             if result:
