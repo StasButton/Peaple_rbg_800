5402cb5255a73a0fd7d51e880f834384956269c0
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-11 17:51:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from tensorflow.keras.preprocessing import image
-#from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
-#--------------------------------------------------
 from tensorflow.keras import utils
 import u_net
 
@@ -38,6 +36,8 @@ def Prediction(i):
     
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение')
+    return  uploaded_file
+    '''
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -48,17 +48,17 @@ def load_image():
             x = preprocess_image(img)
             pred_ar = Prediction(x)
             pred_im  = image.array_to_img(pred_ar)
-            D = st.image(pred_im)
+            st.image(pred_im)
             
             #st.text(pred_ar.shape)
             #st.text(x.shape)
-        return D
+        return  image_data
     else:
         return None
-
+    '''
 st.title('Загрузка, скачивание изображений')
 
-s = load_image()
+#s = load_image()
 
 #if s is not None:
  #   st.download_button(label='скачать',data=s,file_name = 'O.jpg')
