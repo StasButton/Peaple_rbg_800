5cf6007d9f0200bf0f965c1b0255c1a9a1b1ec7d
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-11 16:35:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def load_image():
             #pred_ar = pred_ar.reshape(img_height,img_width)
             pred_im  = image.array_to_img(pred_ar)
             #pred_im = Image.fromarray(pred_ar)
-            #st.image(pred_im)
+            st.image(pred_im)
             st.text(pred_ar.shape)
             st.text(x.shape)
         return image_data
