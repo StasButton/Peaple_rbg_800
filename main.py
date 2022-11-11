f073a2957afc88969ef7dfab7aa566a6da935d7d
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-11 14:47:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,10 +54,10 @@ def load_image():
         result = st.button('Распознать изображение')
         if result:
             x = preprocess_image(img)
-            pred_ar = Prediction(x)
+            #pred_ar = Prediction(x)
             #pred_im = Image.fromarray(pred)
             #st.image(pred_im)
-            st.text(pred_ar.shape)
+            st.text(x.shape)
         #return  x
     else:
         return None
