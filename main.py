ab7728bcecf9983a7cde1273c1f1a66ff165b3c1
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-13 20:12:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ with col3:
             st.text('надо')
             #data = loadresult(x,x_bg)
         result1 = st.button('фон')
+        if result:
             st.text('нахер')
         #st.download_button(label='Скачать готовое изображение',data = data,file_name='change_bg.jpg')
   
