b18f3990f9f90b819a39c1ecddf0439f84ad19a9
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-14 01:14:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ with col3:
         if(len(st.session_state.log) > 0):
             b = True
             st.image(st.session_state.log[-1])
+        st.text(b)
         st.download_button(label='Скачать готовое изображение',data = data,file_name='change_bg.jpg',disabled = b)
 
 #---------------------------
