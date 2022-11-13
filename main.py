c1d7096152fea1c40935a5a5900b84588116ee3a
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-13 06:28:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,16 @@ def load_image():
 
             result = st.button('Заменить фон')
             if result:
-                data = loadresult()               
+                pred_ar = pedict2(x,x_bg) 
+                im = utils.array_to_img(pred_ar)
+                pred_ar_int = pred_ar.astype(np.uint8)
+                im = Image.fromarray(pred_ar_int)
+                st.image(im)
+                
+                with io.BytesIO() as f:
+                    im.save(f, format='JPEG')
+                    data = f.getvalue()
+                #data = loadresult()               
             #st.download_button(label='Скачать',data=data,file_name='change_bg.jpg')
                 
                 
