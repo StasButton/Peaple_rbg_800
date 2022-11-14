862f3d44b4bba9e955eeccfcc01a29aa9a6b7f6e
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-14 23:28:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -64,14 +64,12 @@ with col3:
                  data = f.getvalue()
                  b =  False
 #-----------------------------------------------------------------------
-
-st.sidebar
-'''
-.selectbox(
+#with st.sidebar:
+st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone")
 )
-'''
+
 with st.sidebar:
     add_radio = st.radio(
         "Choose a shipping method",
